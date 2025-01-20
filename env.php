5b1de59197e368f6152c75290d63565be3b63318
rgalvaomachado <rgalvaomachado@gmail.com>
2025-01-20 18:53:50 -0300

    melhorias

--- env.php
+++ env.php
@@ -3,6 +3,7 @@
     $_ENV['DATABASE_USERNAME'] = 'root';
     $_ENV['DATABASE_PASSWORD'] = 'root123';
     $_ENV['DATABASE_HOST'] = 'db';
+    $_ENV['DATABASE_PORT'] = '3306';
     $_ENV['DATABASE_NAME'] = 'capiphp';
 
     $_ENV['TIMEZONE'] = 'America/Sao_Paulo';
